4cbd7eef08621a1160b185a50db3d31b59888742
Harjun751 <Harjun751@gmail.com>
2025-05-28 19:18:53 +0800

    change to BACKEND_ENDPOINT

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,14 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 
-const backendHost = process.env.BACKEND_HOST || '127.0.0.1'
-const backendPort = process.env.BACKEND_PORT || '3001'
+const backendEndpoint = process.env.BACKEND_ENDPOINT || 'http://127.0.0.1:3001'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/questions': `http://${backendHost}:${backendPort}`,
+      '/questions': `${backendEndpoint}`,
     },
   },
 })
