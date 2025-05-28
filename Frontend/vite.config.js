4a15f170f8518dfb4e42f0413f0abca34ab68570
Harjun751 <Harjun751@gmail.com>
2025-05-28 19:47:13 +0800

    rollback

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,26 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 
 const backendEndpoint = process.env.BACKEND_ENDPOINT || 'http://127.0.0.1:3001'
 
-const serverHost = process.env.__VITE_ADDITIONAL_SERVER_ALLOWED_HOSTS;
 
 export default defineConfig({
   plugins: [
       react(),
-      ...(serverHost
-          ? [
-              basicSsl({
-                  name: 'test',
-                  domains: [process.env.__VITE_ADDITIONAL_SERVER_ALLOWED_HOSTS],
-                  certDir: '/cert',
-              })
-          ]
-          : []
-      )
   ],
   server: {
     proxy: {
